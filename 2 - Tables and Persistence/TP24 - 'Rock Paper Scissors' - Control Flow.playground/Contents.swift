b0eb89f6801babc8184fe29a7d12fe71af1e90c8
Ezra Pease <pieezra@gmail.com>
2025-11-07 07:46:50 -0700

    Completed TP24, and with my own version via use of an enum for the answers

--- 2 - Tables and Persistence/TP24 - 'Rock Paper Scissors' - Control Flow.playground/Contents.swift
+++ 2 - Tables and Persistence/TP24 - 'Rock Paper Scissors' - Control Flow.playground/Contents.swift
@@ -19,3 +19,90 @@
     //  Expand your game to include Spock and Lizard as weapon options. You can view the matchups here: 🔗https://www.samkass.com/theories/RPSSL.html
 
 import Foundation
+
+
+// Available Answers for Rock Paper Scissors
+enum RockPaperScissorsAnswers {
+    case rock
+    case paper
+    case scissors
+    case spock
+    case lizard
+}
+
+func rockPaperScissors(player1 input1: RockPaperScissorsAnswers, player2 input2: RockPaperScissorsAnswers) {
+    switch input1 {
+    case .rock:
+        switch input2 {
+        case .rock:
+            print("Its a tie!")
+        case .paper:
+            print("Player 2 wins!")
+        case .scissors:
+            print("Player 1 wins!")
+        case .spock:
+            print("Player 2 wins!")
+        case .lizard:
+            print("Player 1 wins!")
+        }
+    case .paper:
+        switch input2 {
+        case .rock:
+            print("Player 1 wins!")
+        case .paper:
+            print("Its a tie!")
+        case .scissors:
+            print("Player 2 wins!")
+        case .spock:
+            print("Player 1 wins!")
+        case .lizard:
+            print("Player 2 wins!")
+        }
+    case .scissors:
+        switch input2 {
+        case .rock:
+            print("Player 2 wins!")
+        case .paper:
+            print("Player 1 wins!")
+        case .scissors:
+            print("Its a tie!")
+        case .spock:
+            print("Player 2 wins!")
+        case .lizard:
+            print("Player 1 wins!")
+        }
+    case .spock:
+        switch input2 {
+        case .rock:
+            print("Player 2 wins!")
+        case .paper:
+            print("Player 1 wins!")
+        case .scissors:
+            print("Player 1 wins!")
+        case .spock:
+            print("Its a tie!")
+        case .lizard:
+            print("Player 2 wins!")
+        }
+    case .lizard:
+        switch input2 {
+        case .rock:
+            print("Player 2 wins!")
+        case .paper:
+            print("Player 1 wins!")
+        case .scissors:
+            print("Player 2 wins!")
+        case .spock:
+            print("Player 1 wins!")
+        case .lizard:
+            print("Its a tie!")
+        }
+    }
+}
+
+
+rockPaperScissors(player1: .paper, player2: .scissors)
+rockPaperScissors(player1: .rock, player2: .scissors)
+rockPaperScissors(player1: .rock, player2: .rock)
+rockPaperScissors(player1: .scissors, player2: .spock)
+rockPaperScissors(player1: .lizard, player2: .paper)
