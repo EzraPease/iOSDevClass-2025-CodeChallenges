a86c13c2acde6b158957e506e2c9c998598fe7f8
Ezra Pease <pieezra@gmail.com>
2025-11-19 08:10:07 -0700

    Very over engineered, and uncomplete, but made an effort

--- 3 - Networking and Data Storage/ND04 - 'Binary to Decimal' - Type Conversion.playground/Contents.swift
+++ 3 - Networking and Data Storage/ND04 - 'Binary to Decimal' - Type Conversion.playground/Contents.swift
@@ -16,3 +16,34 @@
     //  Add your functions as extensions of String and Int, respectively, so they can be called directly on any String or Int. You will need to also add input validation so you can be certain that a String is in binary format when the user tries to convert it; return nil if the input is invalid.
 
 import Foundation
+
+
+enum BinaryErrors: Error {
+    case invalidInput
+    case unableToConvertToDouble
+}
+
+
+
+
+func binaryToDecimal(binaryInput: String) throws -> Int {
+    var result = 0
+    if let doubleBinary = Double(binaryInput) {
+        print(doubleBinary)
+        
+        for (index, doubleBinary) in binaryInput.enumerated() {
+            print("Binary Number: \(doubleBinary)")
+            print("Index: \(index)")
+            guard doubleBinary == "1" || doubleBinary == "0" else {
+                print("Binary Error: Input must be 1's and 0's \(BinaryErrors.invalidInput)")
+                throw BinaryErrors.invalidInput }
+            result = Int(pow(doubleBinary, index)
+        }
+    } else {
+        throw BinaryErrors.unableToConvertToDouble
+    }
+        
+}
+
+
+try binaryToDecimal(binaryInput: "10010001")
