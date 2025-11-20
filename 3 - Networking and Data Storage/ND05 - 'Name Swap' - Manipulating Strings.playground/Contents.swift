b09f19c2b8478480cfc71e8780af515177d13e65
Ezra Pease <pieezra@gmail.com>
2025-11-20 08:02:19 -0700

    Completed ND05

--- 3 - Networking and Data Storage/ND05 - 'Name Swap' - Manipulating Strings.playground/Contents.swift
+++ 3 - Networking and Data Storage/ND05 - 'Name Swap' - Manipulating Strings.playground/Contents.swift
@@ -16,3 +16,48 @@
     //  Write another function that accepts a full name as input (first, middle, last) and returns a string in the format, "lastName, firstName middleInitial." Include the comma, abbreviate the middle initial to one letter and add a period.
 
 import Foundation
+
+enum Errors: Error {
+    case notFirstMiddleLast
+    case noMiddleInitial
+    case notFirstLast
+}
+
+
+func nameSwap(fullName name: String) throws -> String {
+    var nameArray = name.components(separatedBy: " ")
+    guard nameArray.count == 2 else { throw Errors.notFirstLast }
+   return "\(nameArray[1]) \(nameArray[0])"
+}
+
+
+
+print(try nameSwap(fullName: "Ezra Pease"))
+
+do {
+    try print(nameSwap(fullName: "Billy Bob Joe"))
+} catch {
+    print(error)
+}
+
+
+print("\n")
+// BLACK DIAMOND
+func blackDiamondNameSwap(firstMiddleLast name: String) throws -> String {
+    var nameArray = name.components(separatedBy: " ")
+    guard nameArray.count == 3 else { throw Errors.notFirstMiddleLast }
+    
+    let firstName = nameArray[0]
+    guard let middleInitial = nameArray[1].first else { throw Errors.noMiddleInitial }
+    let lastName = nameArray[2]
+    
+    return "\(lastName), \(firstName) \(middleInitial)."
+}
+
+
+print(try blackDiamondNameSwap(firstMiddleLast: "Ezra Michael Pease"))
+do {
+    try print(blackDiamondNameSwap(firstMiddleLast: "Ezra Pease"))
+} catch {
+    print(error)
+}
