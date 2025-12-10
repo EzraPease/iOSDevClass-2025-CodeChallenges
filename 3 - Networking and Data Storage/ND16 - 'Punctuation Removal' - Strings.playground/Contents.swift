12da28c423c4fac2c00f83f33bbc599bdb1f956a
Ezra Pease <pieezra@gmail.com>
2025-12-10 08:23:18 -0700

    Completed ND16

--- 3 - Networking and Data Storage/ND16 - 'Punctuation Removal' - Strings.playground/Contents.swift
+++ 3 - Networking and Data Storage/ND16 - 'Punctuation Removal' - Strings.playground/Contents.swift
@@ -2,19 +2,48 @@
 //  Concept: Practice manipulating a string
 
 //  Instructions:
-    //  Remove any punctuation mark from the end of a string.
-    //  If the string ends with a punctuation mark (e.g., '.', ',', '!', '?', ';', ':', '-', '_'), remove only the last occurrence of such a mark.
-    //  If the input string is empty, return an empty string.
+//  Remove any punctuation mark from the end of a string.
+//  If the string ends with a punctuation mark (e.g., '.', ',', '!', '?', ';', ':', '-', '_'), remove only the last occurrence of such a mark.
+//  If the input string is empty, return an empty string.
 
 //  Examples:
-    //  Input: "Greetings, friends!!"
-    //  Output: "Greetings, friends!"
+//  Input: "Greetings, friends!!"
+//  Output: "Greetings, friends!"
 
-    //  Input: "Wow."
-    //  Output: "Wow"
+//  Input: "Wow."
+//  Output: "Wow"
 
 //  ⌺ Black Diamond Challenge:
-    //  Create another function. This time, remove all leading and trailing punctuation, including multiples.
-    //  Input: "...This is a good example!!!", Output: "This is a good example"
+//  Create another function. This time, remove all leading and trailing punctuation, including multiples.
+//  Input: "...This is a good example!!!", Output: "This is a good example"
 
 import Foundation
+
+
+
+
+func removePuncuation(from input: String) -> String {
+    var originalInput = input
+    guard !originalInput.isEmpty else { return input }
+    
+    if originalInput.last?.isPunctuation == true {
+        originalInput.removeLast()
+    }
+    return originalInput
+}
+
+print(removePuncuation(from: "Hello!"))
+print(removePuncuation(from: "My Name is Ezra"))
+print(removePuncuation(from: "H#llo.,"))
+print("\n")
+
+
+// BLACK DIAMOND
+func removeAllPuncuation(from input: String) -> String {
+    input.trimmingCharacters(in: CharacterSet.punctuationCharacters)
+}
+
+
+print(removeAllPuncuation(from: "...This is a good example!!!"))
+print(removeAllPuncuation(from: ",.,...,,...,Hello!"))
+print(removeAllPuncuation(from: "Goodbye.,._______"))
