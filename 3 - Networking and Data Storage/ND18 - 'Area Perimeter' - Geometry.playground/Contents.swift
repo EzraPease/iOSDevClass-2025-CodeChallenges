fbe2a031b955383ae444b82af5b24caeb4fe229c
Ezra Pease <pieezra@gmail.com>
2025-12-12 09:07:53 -0700

    Completed ND18

--- 3 - Networking and Data Storage/ND18 - 'Area Perimeter' - Geometry.playground/Contents.swift
+++ 3 - Networking and Data Storage/ND18 - 'Area Perimeter' - Geometry.playground/Contents.swift
@@ -18,3 +18,35 @@
     //  Create a new function that takes three lengths for the three sides of a triangle. Calculate and return its perimeter and its area.
 
 import Foundation
+
+
+
+
+func areaPerimeter(length: Int, width: Int) -> Int {
+    length == width ? length * width : (2 * length) + (2 * width)
+}
+
+
+print(areaPerimeter(length: 10, width: 10))
+print(areaPerimeter(length: 5, width: 2))
+print("\n")
+
+
+
+// BLACK DIAMOND
+func triangleAreaPerimeter(side1: Int, side2: Int, side3: Int) -> (Int, Double) {
+    var perimeter = side1 + side2 + side3
+    let semiPerimeter = perimeter / 2
+    print("Semi Permiter: \(semiPerimeter)")
+    
+    let area = sqrt(Double(semiPerimeter * (semiPerimeter - side1) * (semiPerimeter - side2) * (semiPerimeter - side3)))
+    print("Area: \(area)")
+    if area == 0 {
+        print("Invalid Triangle")
+    }
+    
+    return (perimeter, area)
+}
+
+
+print(triangleAreaPerimeter(side1: 7, side2: 7, side3: 7))
